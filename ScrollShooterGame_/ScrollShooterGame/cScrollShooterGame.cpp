beaf8206a938718301f738775e1d171047382cd9
WaterFriend <116010169@link.cuhk.edu.cn>
2023-11-19 22:32:07 -0700

    Add ScrollShooterGame project to solution

---
+++ ScrollShooterGame_/ScrollShooterGame/cScrollShooterGame.cpp
@@ -0,0 +1,468 @@
+// Includes
+//=========
+
+#include <iostream>
+
+#include "cScrollShooterGame.h"
+
+#include <Engine/Asserts/Asserts.h>
+#include <Engine/Graphics/Graphics.h>
+#include <Engine/Graphics/ConstantBufferFormats.h>
+#include <Engine/Logging/Logging.h>
+#include <Engine/Math/cMatrix_transformation.h>
+#include <Engine/Math/Functions.h>
+#include <Engine/Math/sVector.h>
+#include <Engine/UserInput/UserInput.h>
+
+
+// TODO: Tempory code for collider testing
+#include <Engine/UserOutput/UserOutput.h>
+#include <Engine/Physics/cAABBCollider.h>
+#include <Engine/Physics/Collision.h>
+#include <vector>
+#include <iostream>
+
+
+
+// Inherited Implementation
+//=========================
+
+// Run
+//----
+
+void eae6320::cScrollShooterGame::UpdateBasedOnInput()
+{
+	// Is the user pressing the ESC key?
+	if ( UserInput::IsKeyPressed( UserInput::KeyCodes::Escape ) )
+	{
+		// Exit the application
+		const auto result = Exit( EXIT_SUCCESS );
+		EAE6320_ASSERT( result );
+	}
+}
+
+
+void eae6320::cScrollShooterGame::UpdateSimulationBasedOnInput()
+{
+	m_camera.UpdateBasedOnInput();
+
+	m_renderObject_triangle.UpdateBasedOnInput();
+
+
+	if (UserInput::IsKeyPressed(UserInput::KeyCodes::Enter))
+	{
+		isKeyPressed = true;
+	}
+	else
+	{
+		isKeyPressed = false;
+	}
+
+
+	// TODO: Temporary code for collider debug
+	m_colliderObject_AABB1.UpdateBasedOnInput();
+	//m_colliderObject_sphere2.UpdateBasedOnInput();
+
+}
+
+
+void eae6320::cScrollShooterGame::UpdateSimulationBasedOnTime(const float i_elapsedSecondCount_sinceLastUpdate)
+{
+	m_camera.UpdateBasedOnTime(i_elapsedSecondCount_sinceLastUpdate);
+
+	m_renderObject_triangle.UpdateBasedOnTime(i_elapsedSecondCount_sinceLastUpdate);
+	m_renderObject_Ganyu.UpdateBasedOnTime(i_elapsedSecondCount_sinceLastUpdate);
+	m_renderObject_Keqing.UpdateBasedOnTime(i_elapsedSecondCount_sinceLastUpdate);
+
+
+
+	// TODO: Temporary code for collider debug
+	m_colliderObject_AABB1.UpdateBasedOnTime(i_elapsedSecondCount_sinceLastUpdate);
+	m_colliderObject_AABB2.UpdateBasedOnTime(i_elapsedSecondCount_sinceLastUpdate);
+	m_colliderObject_sphere2.UpdateBasedOnTime(i_elapsedSecondCount_sinceLastUpdate);
+
+	Physics::Collision::Update_CollisionDetection();
+
+	Physics::Collision::Update_CollisionResolution();
+
+}
+
+
+void eae6320::cScrollShooterGame::SubmitDataToBeRendered(
+	const float i_elapsedSecondCount_systemTime, 
+	const float i_elapsedSecondCount_sinceLastSimulationUpdate)
+{
+	// Submit camera data
+	{
+		// aspectRatio = width / height
+		uint16_t width, height;
+		GetCurrentResolution(width, height);
+
+		Graphics::SubmitCameraMatrices(
+			m_camera.CreateWorldToCameraMatrix(i_elapsedSecondCount_sinceLastSimulationUpdate),
+			m_camera.CreateCameraToProjectedMatrix(static_cast<float>(width), static_cast<float>(height)));
+	}
+
+	// Submit background color
+	{
+		Graphics::SubmitBackgroundColor(0.5f, 0.5f, 0.5f);
+	}
+
+
+	// Submit mesh-effect pair data
+	{
+		size_t arraySize = m_renderObjectList.size();
+
+		Graphics::ConstantBufferFormats::sMeshEffectPair* meshEffectPairArray = new Graphics::ConstantBufferFormats::sMeshEffectPair[arraySize];
+		Math::cMatrix_transformation* transformMatrixArray = new Math::cMatrix_transformation[arraySize];
+
+		// Render data of render objects 
+		for (size_t i = 0; i < arraySize; i++)
+		{
+			meshEffectPairArray[i].Initialize(m_renderObjectList[i]->GetMesh(), m_renderObjectList[i]->GetEffect());
+			transformMatrixArray[i] = m_renderObjectList[i]->GetPredictedTransform(i_elapsedSecondCount_sinceLastSimulationUpdate);
+		}
+
+		Graphics::SubmitMeshEffectData(meshEffectPairArray, transformMatrixArray, static_cast<uint32_t>(arraySize));
+
+		// clean up 
+		for (size_t i = 0; i < arraySize; i++)
+		{
+			meshEffectPairArray[i].CleanUp();
+		}
+
+		delete[] meshEffectPairArray;
+		delete[] transformMatrixArray;
+	}
+
+
+	// TODO: Submit debug for box collider
+	{
+		auto BVHRenderData = std::vector<std::pair<eae6320::Graphics::cLine*, eae6320::Math::cMatrix_transformation>>();
+		BVHRenderData = Physics::Collision::GetBVHRenderData();
+
+		size_t staticSize = m_colliderObjectList.size();
+		size_t arraySize = BVHRenderData.size() + staticSize;
+
+		Graphics::ConstantBufferFormats::sDebug* debugDataArray = new Graphics::ConstantBufferFormats::sDebug[arraySize];
+
+		// Render data of hard-coded collider
+		for (size_t i = 0 ; i < m_colliderObjectList.size(); i++)
+		{
+			auto collider = m_colliderObjectList[i];
+			debugDataArray[i].Initialize(collider->GetColliderLine(), collider->GetPredictedTransform(i_elapsedSecondCount_sinceLastSimulationUpdate));
+		}
+
+		// Render data of BVH tree
+		for (size_t i = staticSize; i < arraySize; i++)
+		{
+			debugDataArray[i].Initialize(BVHRenderData[i - staticSize].first, BVHRenderData[i - staticSize].second);
+		}
+
+		Graphics::SubmitDebugData(debugDataArray, static_cast<uint32_t>(arraySize));
+
+		// Clean up
+		for (size_t i = 0; i < arraySize; i++)
+		{
+			debugDataArray[i].CleanUp();
+		}
+
+		delete[] debugDataArray;
+	}
+}
+
+
+// Initialize / Clean Up
+//----------------------
+
+eae6320::cResult eae6320::cScrollShooterGame::Initialize()
+{
+	eae6320::Logging::OutputMessage("\n --------------MoZiheng MyGame customized log messge--------------");
+
+#if defined( EAE6320_PLATFORM_D3D )
+	eae6320::Logging::OutputMessage("Solution Platform: Direct3D");
+#elif defined( EAE6320_PLATFORM_GL )
+	eae6320::Logging::OutputMessage("Solution Platform: OpenGL");
+#endif
+
+#ifdef _WIN64
+	eae6320::Logging::OutputMessage("Machine Platform: x64");
+#else
+	eae6320::Logging::OutputMessage("Machine Platform: x86");
+#endif
+
+#ifdef _DEBUG
+	eae6320::Logging::OutputMessage("Configuration: Debug");
+#else
+	eae6320::Logging::OutputMessage("Configuration: Release");
+#endif
+
+	eae6320::Logging::OutputMessage("Initialization Successfull!! \n");
+
+
+	InitializeCamera();
+
+	InitializeMeshData();
+
+	InitializeGameObject();
+
+	// TODO: temporary code for initialize collision system
+	InitializeCollisionSystem();
+
+
+	return Results::Success;
+}
+
+
+eae6320::cResult eae6320::cScrollShooterGame::CleanUp()
+{
+	eae6320::Logging::OutputMessage("\n --------------MoZiheng MyGame customized log messge--------------");
+
+#if defined( EAE6320_PLATFORM_D3D )
+	eae6320::Logging::OutputMessage("Solution Platform: Direct3D");
+#elif defined( EAE6320_PLATFORM_GL )
+	eae6320::Logging::OutputMessage("Solution Platform: OpenGL");
+#endif
+
+#ifdef _WIN64
+	eae6320::Logging::OutputMessage("Machine Platform: x64");
+#else
+	eae6320::Logging::OutputMessage("Machine Platform: x86");
+#endif
+
+#ifdef _DEBUG
+	eae6320::Logging::OutputMessage("Configuration: Debug");
+#else
+	eae6320::Logging::OutputMessage("Configuration: Release");
+#endif
+
+	eae6320::Logging::OutputMessage("Clan Up Successfull!!\n");
+
+	CleanUpGameObject();
+
+	return Results::Success;
+}
+
+
+void eae6320::cScrollShooterGame::InitializeMeshData()
+{
+	Graphics::cEffect::Create(m_effect_animate, m_vertexShaderPath, m_fragmentShaderPath_animate);
+
+	Graphics::cEffect::Create(m_effect_standard, m_vertexShaderPath, m_fragmentShaderPath_standard);
+}
+
+
+void eae6320::cScrollShooterGame::InitializeCamera()
+{
+	m_camera.Initialize(
+		0.0f, 0.0f, 5.0f,
+		Math::ConvertDegreesToRadians(45.0f), 
+		0.1f, 
+		100.0f);
+
+	//m_camera.InitializeMesh(m_rectangleMeshPath);
+	//m_camera.InitializeEffect(m_vertexShaderPath, m_fragmentShaderPath_animate);
+}
+
+
+void eae6320::cScrollShooterGame::InitializeGameObject()
+{
+	// Game object initialization
+	{
+		// Tirangle
+		m_renderObject_triangle.InitializeMesh(m_triangleMeshPath);
+		m_renderObject_triangle.InitializeEffect(m_vertexShaderPath, m_fragmentShaderPath_standard);
+		m_renderObject_triangle.GetRigidBody().angularSpeed = 1.0f;
+		m_renderObject_triangle.GetRigidBody().angularVelocity_axis_local = Math::sVector(0.0f, 0.0f, 1.0f);
+
+		// Rectangle
+		m_renderObject_rectangle.InitializeMesh(m_rectangleMeshPath);
+		m_renderObject_rectangle.InitializeEffect(m_vertexShaderPath, m_fragmentShaderPath_standard);
+		m_renderObject_rectangle.GetRigidBody().orientation = Math::cQuaternion(0.5f, Math::sVector(1, 0, 1));
+		m_renderObject_rectangle.GetRigidBody().position = Math::sVector(+0.0f, -1.0f, +0.5f);
+
+		// Plane
+		m_renderObject_plane.InitializeMesh(m_planeMeshPath);
+		m_renderObject_plane.InitializeEffect(m_vertexShaderPath, m_fragmentShaderPath_standard);
+		m_renderObject_plane.GetRigidBody().position = Math::sVector(+6.0f, -5.0f, -3.0f);
+
+		// Cube
+		m_renderObject_cube.InitializeMesh(m_cubeMeshPath);
+		m_renderObject_cube.InitializeEffect(m_vertexShaderPath, m_fragmentShaderPath_animate);
+		m_renderObject_cube.GetRigidBody().position = Math::sVector(-2.0f, -2.0f, -3.0f);
+
+		// Keqing
+		m_renderObject_Keqing.InitializeMesh(m_keqingMeshPath);
+		m_renderObject_Keqing.InitializeEffect(m_vertexShaderPath, m_fragmentShaderPath_standard);
+		m_renderObject_Keqing.GetRigidBody().position = Math::sVector(-5.0f, -10.0f, -20.0f);
+
+		// Keqing - skin
+		m_renderObject_Keqing_skin.InitializeMesh(m_keqing_SkinMeshPath);
+		m_renderObject_Keqing_skin.InitializeEffect(m_vertexShaderPath, m_fragmentShaderPath_standard);
+		m_renderObject_Keqing_skin.GetRigidBody().position = Math::sVector(-5.0f, -10.0f, -20.0f);
+
+		// Ganyu
+		m_renderObject_Ganyu.InitializeMesh(m_ganyuMeshPath);
+		m_renderObject_Ganyu.InitializeEffect(m_vertexShaderPath, m_fragmentShaderPath_standard);
+		m_renderObject_Ganyu.GetRigidBody().position = Math::sVector(5.0f, -10.0f, -20.0f);
+
+
+
+		m_renderObjectList.push_back(&m_renderObject_triangle);
+		m_renderObjectList.push_back(&m_renderObject_rectangle);
+		m_renderObjectList.push_back(&m_renderObject_plane);
+		m_renderObjectList.push_back(&m_renderObject_cube);
+		m_renderObjectList.push_back(&m_renderObject_Keqing);
+		//m_renderObjectList.push_back(&m_renderObject_Keqing_skin);
+		m_renderObjectList.push_back(&m_renderObject_Ganyu);
+	}
+
+
+	// TODO: temporary code for initialize colldier object
+	{
+		Physics::sColliderSetting setting_AABB1;
+		setting_AABB1.SettingForAABB(Math::sVector(-0.5, -0.5, -0.5), Math::sVector(0.5, 0.5, 0.5));
+
+		Physics::sColliderSetting setting_sphere1;
+		setting_sphere1.SettingForSphere(Math::sVector(0, 0, 0), 0.5f);
+
+
+		m_colliderObject_AABB1.GetRigidBody().position = Math::sVector(0.5f, 0.0f, 0.5f);
+		m_colliderObject_AABB1.InitializeCollider(setting_AABB1);
+		m_colliderObject_AABB1.InitializeColliderLine();
+		m_colliderObject_AABB1.GetCollider()->m_name = "AABB_1";
+		m_colliderObject_AABB1.GetCollider()->OnCollisionEnter = 
+			[this](Physics::cCollider* self, Physics::cCollider* other) -> void { UserOutput::ConsolePrint(" Enter collision, other: ", other->m_name.c_str()); m_colliderObject_AABB1.SetIsCollide(true); };
+		m_colliderObject_AABB1.GetCollider()->OnCollisionStay = 
+			[this](Physics::cCollider* self, Physics::cCollider* other) -> void { UserOutput::ConsolePrint(std::string("Stay collision, " + self->m_name + " : " ).c_str(), other->m_name.c_str()); };
+		m_colliderObject_AABB1.GetCollider()->OnCollisionExit = 
+			[this](Physics::cCollider* self, Physics::cCollider* other) -> void { UserOutput::ConsolePrint(" Exit collision, other: ", other->m_name.c_str()); m_colliderObject_AABB1.SetIsCollide(false); };
+
+		m_colliderObject_AABB2.GetRigidBody().position = Math::sVector(-1.0f, -0.5f, 1.5f);
+		m_colliderObject_AABB2.InitializeCollider(setting_AABB1);
+		m_colliderObject_AABB2.InitializeColliderLine();
+		m_colliderObject_AABB2.GetCollider()->m_name = "AABB_2";
+		m_colliderObject_AABB2.GetCollider()->OnCollisionEnter = 
+			[this](Physics::cCollider* self, Physics::cCollider* other) -> void { UserOutput::ConsolePrint(" Enter collision, other: ", other->m_name.c_str()); m_colliderObject_AABB2.SetIsCollide(true); };
+		m_colliderObject_AABB2.GetCollider()->OnCollisionStay =
+			[this](Physics::cCollider* self, Physics::cCollider* other) -> void { UserOutput::ConsolePrint(std::string("Stay collision, " + self->m_name + " : ").c_str(), other->m_name.c_str()); };
+		m_colliderObject_AABB2.GetCollider()->OnCollisionExit = 
+			[this](Physics::cCollider* self, Physics::cCollider* other) -> void { UserOutput::ConsolePrint(" Exit collision, other: ", other->m_name.c_str()); m_colliderObject_AABB2.SetIsCollide(false); };
+
+		m_colliderObject_AABB3.GetRigidBody().position = Math::sVector(1.0f, -2.0f, 3.0f);
+		m_colliderObject_AABB3.InitializeCollider(setting_AABB1);
+		m_colliderObject_AABB3.InitializeColliderLine();
+		m_colliderObject_AABB3.GetCollider()->m_name = "AABB_3";
+		m_colliderObject_AABB3.GetCollider()->OnCollisionEnter = 
+			[this](Physics::cCollider* self, Physics::cCollider* other) -> void { UserOutput::ConsolePrint(" Enter collision, other: ", other->m_name.c_str()); m_colliderObject_AABB3.SetIsCollide(true); };
+		m_colliderObject_AABB3.GetCollider()->OnCollisionStay =
+			[this](Physics::cCollider* self, Physics::cCollider* other) -> void { UserOutput::ConsolePrint(std::string("Stay collision, " + self->m_name + " : ").c_str(), other->m_name.c_str()); };
+		m_colliderObject_AABB3.GetCollider()->OnCollisionExit = 
+			[this](Physics::cCollider* self, Physics::cCollider* other) -> void { UserOutput::ConsolePrint(" Exit collision, other: ", other->m_name.c_str()); m_colliderObject_AABB3.SetIsCollide(false); };
+
+		m_colliderObject_AABB4.GetRigidBody().position = Math::sVector(-1.5f, -2.0f, 1.0f);
+		m_colliderObject_AABB4.GetRigidBody().isStatic = true;
+		m_colliderObject_AABB4.InitializeCollider(setting_AABB1);
+		m_colliderObject_AABB4.InitializeColliderLine();
+		m_colliderObject_AABB4.GetCollider()->m_name = "AABB_4";
+		m_colliderObject_AABB4.GetCollider()->OnCollisionEnter =
+			[this](Physics::cCollider* self, Physics::cCollider* other) -> void { UserOutput::ConsolePrint(" Enter collision, other: ", other->m_name.c_str()); m_colliderObject_AABB4.SetIsCollide(true); };
+		m_colliderObject_AABB4.GetCollider()->OnCollisionStay =
+			[this](Physics::cCollider* self, Physics::cCollider* other) -> void { UserOutput::ConsolePrint(std::string("Stay collision, " + self->m_name + " : ").c_str(), other->m_name.c_str()); };
+		m_colliderObject_AABB4.GetCollider()->OnCollisionExit =
+			[this](Physics::cCollider* self, Physics::cCollider* other) -> void { UserOutput::ConsolePrint(" Exit collision, other: ", other->m_name.c_str()); m_colliderObject_AABB4.SetIsCollide(false); };
+
+
+		m_colliderObject_sphere1.GetRigidBody().position = Math::sVector(2.0f, -1.0f, 3.0f);
+		m_colliderObject_sphere1.InitializeCollider(setting_sphere1);
+		m_colliderObject_sphere1.InitializeColliderLine();
+		m_colliderObject_sphere1.GetCollider()->m_name = "Sphere_1";
+		m_colliderObject_sphere1.GetCollider()->OnCollisionEnter = 
+			[this](Physics::cCollider* self, Physics::cCollider* other) -> void { UserOutput::ConsolePrint(" Enter collision, other: ", other->m_name.c_str()); m_colliderObject_sphere1.SetIsCollide(true); };
+		m_colliderObject_sphere1.GetCollider()->OnCollisionStay = 
+			[this](Physics::cCollider* self, Physics::cCollider* other) -> void { UserOutput::ConsolePrint(std::string("Stay collision, " + self->m_name + " : ").c_str(), other->m_name.c_str()); };
+		m_colliderObject_sphere1.GetCollider()->OnCollisionExit = 
+			[this](Physics::cCollider* self, Physics::cCollider* other) -> void { UserOutput::ConsolePrint(" Exit collision, other: ", other->m_name.c_str()); m_colliderObject_sphere1.SetIsCollide(false); };
+	
+		m_colliderObject_sphere2.GetRigidBody().position = Math::sVector(2.0f, 1.5f, 1.0f);
+		m_colliderObject_sphere2.InitializeCollider(setting_sphere1);
+		m_colliderObject_sphere2.InitializeColliderLine();
+		m_colliderObject_sphere2.GetCollider()->m_name = "Sphere_2";
+		m_colliderObject_sphere2.GetCollider()->OnCollisionEnter =
+			[this](Physics::cCollider* self, Physics::cCollider* other) -> void { UserOutput::ConsolePrint(" Enter collision, other: ", other->m_name.c_str()); m_colliderObject_sphere2.SetIsCollide(true); };
+		m_colliderObject_sphere2.GetCollider()->OnCollisionStay =
+			[this](Physics::cCollider* self, Physics::cCollider* other) -> void { UserOutput::ConsolePrint(std::string("Stay collision, " + self->m_name + " : ").c_str(), other->m_name.c_str()); };
+		m_colliderObject_sphere2.GetCollider()->OnCollisionExit =
+			[this](Physics::cCollider* self, Physics::cCollider* other) -> void { UserOutput::ConsolePrint(" Exit collision, other: ", other->m_name.c_str()); m_colliderObject_sphere2.SetIsCollide(false); };
+
+		m_colliderObject_sphere3.GetRigidBody().position = Math::sVector(1.5f, 0.0f, 2.0f);
+		m_colliderObject_sphere3.GetRigidBody().isTrigger = true;
+		m_colliderObject_sphere3.InitializeCollider(setting_sphere1);
+		m_colliderObject_sphere3.InitializeColliderLine();
+		m_colliderObject_sphere3.GetCollider()->m_name = "Sphere_3";
+		m_colliderObject_sphere3.GetCollider()->OnCollisionEnter =
+			[this](Physics::cCollider* self, Physics::cCollider* other) -> void { UserOutput::ConsolePrint(" Enter collision, other: ", other->m_name.c_str()); m_colliderObject_sphere3.SetIsCollide(true); };
+		m_colliderObject_sphere3.GetCollider()->OnCollisionStay =
+			[this](Physics::cCollider* self, Physics::cCollider* other) -> void { UserOutput::ConsolePrint(std::string("Stay collision, " + self->m_name + " : ").c_str(), other->m_name.c_str()); };
+		m_colliderObject_sphere3.GetCollider()->OnCollisionExit =
+			[this](Physics::cCollider* self, Physics::cCollider* other) -> void { UserOutput::ConsolePrint(" Exit collision, other: ", other->m_name.c_str()); m_colliderObject_sphere3.SetIsCollide(false); };
+
+
+
+		m_colliderObjectList.push_back(&m_colliderObject_AABB1);
+		m_colliderObjectList.push_back(&m_colliderObject_AABB2);
+		m_colliderObjectList.push_back(&m_colliderObject_AABB3);
+		m_colliderObjectList.push_back(&m_colliderObject_AABB4);
+		m_colliderObjectList.push_back(&m_colliderObject_sphere1);
+		m_colliderObjectList.push_back(&m_colliderObject_sphere2);
+		m_colliderObjectList.push_back(&m_colliderObject_sphere3);
+	}
+}
+
+
+void eae6320::cScrollShooterGame::CleanUpGameObject()
+{
+	//m_renderObject_triangle.CleanUp();
+	//m_renderObject_rectangle.CleanUp();
+	////m_renderObject_plane.CleanUp();
+	//m_renderObject_cube.CleanUp();
+	//m_renderObject_Keqing.CleanUp();
+	m_renderObject_Keqing_skin.CleanUp();
+	//m_renderObject_Ganyu.CleanUp();
+	for (cGameObject* renderObject : m_renderObjectList)
+	{
+		renderObject->CleanUp();
+	}
+
+
+
+
+	m_camera.CleanUp();
+
+	m_effect_animate->DecrementReferenceCount();
+	m_effect_standard->DecrementReferenceCount();
+
+
+	// TODO: temporary code for clean up colldier object
+	for (cGameObject* colliderObject : m_colliderObjectList)
+	{
+		colliderObject->CleanUp();
+	}
+}
+
+
+
+void eae6320::cScrollShooterGame::InitializeCollisionSystem()
+{
+	std::vector<Physics::cCollider*> colliderList;
+
+	for (cGameObject* colliderObject : m_colliderObjectList)
+	{
+		colliderList.push_back(colliderObject->GetCollider());
+	}
+
+	Physics::Collision::Initialize(colliderList, Physics::Collision::BroadPhase_BVH | Physics::Collision::NarrowPhase_Overlaps);
+}
+
+
+ 
---
+++ ScrollShooterGame_/ScrollShooterGame/cScrollShooterGame.h
@@ -0,0 +1,204 @@
+/*
+	This class is your specific game
+*/
+
+#ifndef EAE6320_CMYGAME_H
+#define EAE6320_CMYGAME_H
+
+// Includes
+//=========
+
+#include <Engine/Application/iApplication.h>
+#include <Engine/Camera/cCamera.h>
+#include <Engine/GameObject/cGameObject.h>
+#include <Engine/Physics/cRigidBody.h>
+#include <Engine/Results/Results.h>
+
+
+#if defined( EAE6320_PLATFORM_WINDOWS )
+	#include "Resource Files/Resource.h"
+#endif
+
+
+
+// TODO: Tempory code for collider testing
+#include <Engine/Graphics/cMesh.h>
+#include <Engine/Graphics/cEffect.h>
+#include <Engine/Physics/cColliderBase.h>
+#include <Engine/Physics/cSphereCollider.h>
+
+
+
+
+
+// Class Declaration
+//==================
+
+namespace eae6320
+{
+	class cScrollShooterGame final : public Application::iApplication
+	{
+		// Inherited Implementation
+		//=========================
+
+	private:
+
+		// Configuration
+		//--------------
+
+#if defined( EAE6320_PLATFORM_WINDOWS )
+		// The main window's name will be displayed as its caption (the text that is displayed in the title bar).
+		// You can make it anything that you want, but please keep the platform name and debug configuration at the end
+		// so that it's easy to tell at a glance what kind of build is running.
+		const char* GetMainWindowName() const final
+		{
+			return "MoZheng's MyGame"
+				" -- "
+#if defined( EAE6320_PLATFORM_D3D )
+				"Direct3D"
+#elif defined( EAE6320_PLATFORM_GL )
+				"OpenGL"
+#endif
+#ifdef _DEBUG
+				" -- Debug"
+#endif
+			;
+		}
+		// Window classes are almost always identified by name;
+		// there is a unique "ATOM" associated with them,
+		// but in practice Windows expects to use the class name as an identifier.
+		// If you don't change the name below
+		// your program could conceivably have problems if it were run at the same time on the same computer
+		// as one of your classmate's.
+		// You don't need to worry about this for our class,
+		// but if you ever ship a real project using this code as a base you should set this to something unique
+		// (a generated GUID would be fine since this string is never seen)
+		const char* GetMainWindowClassName() const final { return "MoZheng's EAE6320 Main Window Class"; }
+		// The following three icons are provided:
+		//	* IDI_EAEGAMEPAD
+		//	* IDI_EAEALIEN
+		//	* IDI_VSDEFAULT_LARGE / IDI_VSDEFAULT_SMALL
+		// If you want to try creating your own a convenient website that will help is: http://icoconvert.com/
+		const WORD* GetLargeIconId() const final { static constexpr WORD iconId_large = IDI_EAEGAMEPAD; return &iconId_large; }
+		const WORD* GetSmallIconId() const final { static constexpr WORD iconId_small = IDI_EAEGAMEPAD; return &iconId_small; }
+#endif
+
+		// Run
+		//----
+
+		void UpdateBasedOnInput() final;
+
+		void UpdateSimulationBasedOnInput() final;
+
+		void UpdateSimulationBasedOnTime(const float i_elapsedSecondCount_sinceLastUpdate) final;
+
+		void SubmitDataToBeRendered(
+			const float i_elapsedSecondCount_systemTime, 
+			const float i_elapsedSecondCount_sinceLastSimulationUpdate) final;
+
+
+		// Initialize / Clean Up
+		//----------------------
+
+		cResult Initialize() final;
+		cResult CleanUp() final;
+
+
+		// Data
+		//=========================
+
+	private:
+
+		std::string m_vertexShaderPath = "data/Shaders/Vertex/standard.shader";
+		std::string m_fragmentShaderPath_animate = ("data/Shaders/Fragment/moziheng.shader");
+		std::string m_fragmentShaderPath_standard = ("data/Shaders/Fragment/standard.shader");
+
+
+		std::string m_triangleMeshPath = "data/meshes/mesh_triangle.mesh";
+		std::string m_rectangleMeshPath = "data/meshes/mesh_rectangle.mesh";
+		std::string m_planeMeshPath = "data/meshes/mesh_plane.mesh";
+		std::string m_cubeMeshPath = "data/meshes/mesh_cube.mesh";
+		std::string m_keqingMeshPath = "data/meshes/mesh_keqing.mesh";
+		std::string m_keqing_SkinMeshPath = "data/meshes/mesh_keqing_skin.mesh";
+		std::string m_ganyuMeshPath = "data/meshes/mesh_ganyu.mesh";
+
+		//std::string m_triangleMeshPath = "data/meshes/mesh_triangle.lua";
+		//std::string m_rectangleMeshPath = "data/meshes/mesh_rectangle.lua";
+		//std::string m_planeMeshPath = "data/meshes/mesh_plane.lua";
+		//std::string m_cubeMeshPath = "data/meshes/mesh_cube.lua";
+		//std::string m_keqingMeshPath = "data/meshes/mesh_keqing.lua";
+		//std::string m_keqing_SkinMeshPath = "data/meshes/mesh_keqing_skin.lua";
+		//std::string m_ganyuMeshPath = "data/meshes/mesh_ganyu.lua";
+
+		Graphics::cEffect* m_effect_animate = nullptr;
+		Graphics::cEffect* m_effect_standard = nullptr;
+
+		Camera::cCamera m_camera;
+
+		cGameObject m_renderObject_triangle;
+		cGameObject m_renderObject_rectangle;
+		cGameObject m_renderObject_plane;
+		cGameObject m_renderObject_cube;
+		cGameObject m_renderObject_Keqing;
+		cGameObject m_renderObject_Keqing_skin;
+		cGameObject m_renderObject_Ganyu;
+
+		std::vector<cGameObject*> m_renderObjectList;
+
+
+		bool isKeyPressed = false;
+
+		void InitializeMeshData();
+
+		void InitializeCamera();
+
+		void InitializeGameObject();
+
+		void CleanUpGameObject();
+
+
+
+
+
+		// TODO: temporary colldier object
+
+		cGameObject m_colliderObject_AABB1;
+		cGameObject m_colliderObject_AABB2;
+		cGameObject m_colliderObject_AABB3;
+		cGameObject m_colliderObject_AABB4;
+
+		cGameObject m_colliderObject_sphere1;
+		cGameObject m_colliderObject_sphere2;
+		cGameObject m_colliderObject_sphere3;
+
+		std::vector<cGameObject*> m_colliderObjectList;
+
+		void InitializeCollisionSystem();
+
+
+
+	};
+}
+
+// Result Definitions
+//===================
+
+namespace eae6320
+{
+namespace Results
+{
+namespace Application
+{  
+	// You can add specific results for your game here:
+	//	* The System should always be Application
+	//	* The __LINE__ macro is used to make sure that every result has a unique ID.
+	//		That means, however, that all results _must_ be defined in this single file
+	//		or else you could have two different ones with equal IDs.
+	//	* Note that you can define multiple Success codes.
+	//		This can be used if the caller may want to know more about how a function succeeded.
+	constexpr cResult ExampleResult( IsFailure, eSystem::Application, __LINE__, Severity::Default );
+}
+}
+}
+
+#endif	// EAE6320_CEXAMPLEGAME_H
