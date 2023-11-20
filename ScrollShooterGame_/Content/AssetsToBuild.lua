7b2c369e6b352d0a50c314ed44c2f389b989a1b4
WaterFriend <116010169@link.cuhk.edu.cn>
2023-11-19 22:52:07 -0700

    Add ScrollShooterGame project to solution

---
+++ ScrollShooterGame_/Content/AssetsToBuild.lua
@@ -0,0 +1,27 @@
+--[[
+	This file lists every asset that must be built by the AssetBuildSystem
+]]
+
+return
+{
+	shaders =
+	{
+		{ path = "Shaders/Fragment/moziheng.shader", arguments = { "fragment" } },
+	
+		{ path = "Shaders/Vertex/standard.shader", arguments = { "vertex" } },
+		{ path = "Shaders/Fragment/standard.shader", arguments = { "fragment" } },
+
+		{ path = "Shaders/Vertex/vertexInputLayout_mesh.shader", arguments = { "vertex" } },
+	},
+
+	meshes =
+	{
+		{ path = "Meshes/mesh_rectangle.lua" },
+		{ path = "Meshes/mesh_triangle.lua" },
+		{ path = "Meshes/mesh_plane.lua" },
+		{ path = "Meshes/mesh_cube.lua" },
+		{ path = "Meshes/mesh_keqing.lua" },
+		{ path = "Meshes/mesh_keqing_skin.lua" },
+		{ path = "Meshes/mesh_ganyu.lua" },
+	},
+}
