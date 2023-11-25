ec43c8b58069ee5683b0cc6ce8a97243aa7136ac
WaterFriend <116010169@link.cuhk.edu.cn>
2023-11-25 03:44:04 -0700

    Develop cBullet_Enemy

--- ScrollShooterGame_/ScrollShooterGame/cBullet.Enemy.cpp
+++ ScrollShooterGame_/ScrollShooterGame/cBullet.Enemy.cpp
@@ -6,7 +6,7 @@
 #include <Engine/Physics/Collision.h>
 #include <Engine/UserOutput/UserOutput.h>
 
-#include <ScrollShooterGame_/ScrollShooterGame/cEnemy.h>
+#include <ScrollShooterGame_/ScrollShooterGame/cPlayer.h>
 #include <ScrollShooterGame_/ScrollShooterGame/cBullet.Enemy.h>
 #include <ScrollShooterGame_/ScrollShooterGame/cBullet.Player.h>
 #include <ScrollShooterGame_/ScrollShooterGame/cScrollShooterGame.h>
@@ -17,7 +17,7 @@ using namespace eae6320;
 // Interface
 //=========================
 
-void ScrollShooterGame::cBullet_Player::Initialize(
+void ScrollShooterGame::cBullet_Enemy::Initialize(
 	eae6320::Math::sVector i_position,
 	eae6320::Math::sVector i_velocity)
 {
@@ -48,10 +48,10 @@ void ScrollShooterGame::cBullet_Player::Initialize(
 			{
 				m_isCollide = true;
 
-				if (dynamic_cast<cEnemy*>(other->m_gameobject) != nullptr ||
-					dynamic_cast<cBullet_Enemy*>(other->m_gameobject) != nullptr)
+				if (dynamic_cast<cPlayer*>(other->m_gameobject) != nullptr ||
+					dynamic_cast<cBullet_Player*>(other->m_gameobject) != nullptr)
 				{
-					UserOutput::ConsolePrint("Player bullet hit enemy!! \n");
+					UserOutput::ConsolePrint("Enemy bullet hit player!! \n");
 					cScrollShooterGame::Instance()->AddGameObjectCleanUpTask(self->m_gameobject);
 				}
 			};
@@ -67,7 +67,7 @@ void ScrollShooterGame::cBullet_Player::Initialize(
 }
 
 
-void ScrollShooterGame::cBullet_Player::CleanUp()
+void ScrollShooterGame::cBullet_Enemy::CleanUp()
 {
 	auto game = cScrollShooterGame::Instance();
 
---
+++ ScrollShooterGame_/ScrollShooterGame/cBullet.Enemy.h
@@ -0,0 +1,42 @@
+#pragma once
+
+// Includes
+//========
+
+#include <Engine/Math/sVector.h>
+
+#include <ScrollShooterGame_/ScrollShooterGame/cBullet.h>
+
+
+namespace ScrollShooterGame
+{
+
+	class cBullet_Enemy : public cBullet
+	{
+
+		// Interface
+		//=========================
+
+	public:
+
+		// Initialization / Clean Up
+		//--------------------------
+
+		void Initialize(
+			eae6320::Math::sVector i_position = eae6320::Math::sVector(),
+			eae6320::Math::sVector i_velocity = eae6320::Math::sVector());
+
+		void CleanUp() override;
+
+
+		// Data
+		//=========================
+
+	private:
+
+		eae6320::Math::sVector m_velocity = eae6320::Math::sVector(0, 1, 0);
+
+
+	};
+
+}
